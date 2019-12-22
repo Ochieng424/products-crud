7aec0f7bd2a8e9ccd7e64df488f15dca8d1de170
Ochieng424 <ochiengderrick424@gmail.com>
2019-12-22 12:16:19 +0300

    with layout skeleton

---
+++ resources/js/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Ecommerce CRUD</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <router-link :to="{path: '/'}" class="nav-link">Home</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link :to="{path: '/login'}" class="nav-link">Login</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link :to="{path: '/register'}" class="nav-link">Register</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="panel-body">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+
+</style>
